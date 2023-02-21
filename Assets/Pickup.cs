12ae43feb4ca16bcb3b20313729213d692cba506
Victorarsta <victorarsta@gmail.com>
2023-02-21 09:30:51 +0100

    ere

--- Assets/Pickup.cs
+++ Assets/Pickup.cs
@@ -1,30 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class Pickup : MonoBehaviour
 {
+    public GameObject PickupText;
 
+    public bool PickupAvalible;
+    public Vector3 StonePos;
     // Start is called before the first frame update
     void Start()
     {
+        PickupAvalible = false;
+        PickupText.SetActive(false);
         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-    }
-    private void OnTriggerStay2D(Collider2D collision)
-    {
-        if (collision.gameObject.tag == "Player" && Input.GetKeyDown(KeyCode.F) && FindObjectOfType<PlayerMovement>().HasRock == false) 
+        if (PickupAvalible == true)
         {
-            FindObjectOfType<PlayerMovement>().Rock();
-            Destroy(this.gameObject);
+            PickupText.SetActive(true);
+        }
+        if (PickupAvalible == false)
+        {
+            PickupText.SetActive(false);
         }
 
+        if (Input.GetKeyDown(KeyCode.F) && PickupAvalible == true)
+        {
+            FindObjectOfType<PlayerMovement>().HasRock = true;
+            Destroy(this.gameObject);
+        }
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && FindObjectOfType<PlayerMovement>().HasRock == false)
+        {
+            StonePos = new Vector3(gameObject.transform.position.x, gameObject.transform.position.y + 2.5f, gameObject.transform.position.z);
+            PickupAvalible = true;
+            PickupText.transform.position = StonePos;
+            
+
+        }
+        
+
         //Picks up rock when player is touching the rock and presses F.
+    }
+    public void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            PickupAvalible = false;
+        }
     }
 
     //Victor's script.
